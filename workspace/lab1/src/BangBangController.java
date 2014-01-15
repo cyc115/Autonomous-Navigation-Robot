80c3520632d12b257b4dfa2393042e5b9bcff60f
cyc115 <yuechuan20@gmail.com>
2014-01-15 12:36:21 -0500

    added comment with minor change

--- workspace/lab1/src/BangBangController.java
+++ workspace/lab1/src/BangBangController.java
@@ -6,15 +6,20 @@ public class BangBangController implements UltrasonicController{
 	private final int BAND_CENTER, 	// ideal distance from the wall 
 						BANDWIDTH;	// The epsilon 
 	private final int MOTOR_LOW, MOTOR_HIGH; 	//speed of motors
-	
+
 	private final int MOTOR_STRAIGHT = 200;//speed of the motor when going straight 
+	private final int INCREMENT = MOTOR_STRAIGHT/20; //5% INCREMENT
 	
 	private final NXTRegulatedMotor 
 			leftMotor = Motor.A,
 			rightMotor = Motor.C;
+
+	private final NXTRegulatedMotor sensorMotor = Motor.B; 	//Motor used to move sensor
 	
-	private int distance;		
-	private int currentLeftSpeed;
+	private int lftDist;	//distance measured from left wall	
+	private int topDist;	//dist measured from top wall 
+	private int currentDist; 	//stores the latest distance reading, should be equal to either one of the above two 
+	private int currentLeftSpeed;	
 	
 	public BangBangController(int bandCenter, int bandwith, int motorLow, int motorHigh) {
 		//Default Constructor
@@ -29,18 +34,66 @@ public class BangBangController implements UltrasonicController{
 		currentLeftSpeed = 0;
 	}
 	
-	@Override
+	/**
+	 * Fault tolerance for gaps not yet implemented.
+	 * Sensor motion not yet implemented
+	 */
 	public void processUSData(int distance) {
-		this.distance = distance;
-		//if error is small
-		if (Math.abs(distance - BAND_CENTER ) < BANDWIDTH){
-//			leftMotor.s
+		while (true){
+			//first rotate to default (left facing location)
+			sensorMotor.rotateTo(-90);
+			
 		}
 		
-	}
+		
+		
 
-	@Override
+/*
+		while (true){
+			//first rotate to default (left facing location)
+			sensorMotor.rotateTo(-90);
+			this.distance = distance;
+			//if too far
+			if ((distance - BAND_CENTER ) > BANDWIDTH){
+				leftMotor.setSpeed(leftMotor.getSpeed() + INCREMENT);
+				rightMotor.setSpeed(rightMotor.getSpeed() - INCREMENT);
+			}
+			//if too near
+			else if ((distance - BAND_CENTER) < BANDWIDTH){
+				leftMotor.setSpeed(leftMotor.getSpeed() - INCREMENT);
+				rightMotor.setSpeed(rightMotor.getSpeed() + INCREMENT);
+			}
+			else{
+				//do nothing
+				}
+			
+			//rotate to front 
+			sensorMotor.rotateTo(0);
+			if 
+		}
+		
+*/
+
+		
+		
+		
+	}
+	
+	/**
+	 * move sensor left 90 deg
+	 */
+	private void moveSensorLeft(){
+		sensorMotor.rotate(-90);
+	}
+	
+	/**
+	 * move sensor right 90 deg
+	 */
+	private void moveSensorRight(){
+		sensorMotor.rotate(90);
+	}
+	
 	public int readUSDistance() {
-		return this.distance;
+		return this.currentDist;
 	}
 }
--- workspace/lab1/src/UltrasonicController.java
+++ workspace/lab1/src/UltrasonicController.java
@@ -3,5 +3,8 @@ public interface UltrasonicController {
 	
 	public void processUSData(int distance);
 	
+	/**
+	 * @return returns the last read Distance from the ultrasonic sensor 
+	 */
 	public int readUSDistance();
 }
